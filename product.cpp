2cc453c0d44da328a29d44c3ab1b03a860cd03a6
amruth0098 <86010025+amruth0098@users.noreply.github.com>
2022-01-03 13:14:20 +0530

    Add files via upload

---
+++ product.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+   float x,y;
+   
+  cout<<"enter the two numbers"<<endl;
+  cin>>x>>y;
+  cout<<"the product  is "<<x*y;
+
+    return 0;
+}
